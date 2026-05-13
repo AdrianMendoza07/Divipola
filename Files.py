0f92a7a92618e555b18e98101c5747d9e5e42069
Adrian Mendoza <68600509+AdrianMendoza07@users.noreply.github.com>
2026-05-13 16:55:52 +0000

    dsa

--- Files.py
+++ Files.py
@@ -6,19 +6,22 @@ from city import City
 
 
 class Files():
-    def read_file(self, path):
+    def read_file(self, file_path):
 
-        multilista = LinkedList()
+        countries = LinkedList()
+
         colombia = Country("CO", "Colombia")
-        multilista.append(colombia)
 
-        departamentos = {}
+        countries.append(colombia)
 
-        with open(path, encoding="utf-8-sig") as file:
-            
+        departments = {}
+
+        with open(file_path, encoding="utf-8-sig") as file:
+
             reader = csv.DictReader(file)
 
             for row in reader:
+
                 dept_code = row["cod_depto"]
                 dept_name = row["departamento"]
 
@@ -27,52 +30,55 @@ class Files():
 
                 lat = row["lat"]
                 lon = row["lon"]
-                
-                if dept_code not in departamentos:
-                    department = Departament(dept_code, dept_name)
 
-                    multilista.add_child(
+                if dept_code not in departments:
+
+                    department = Departament(
+                        dept_code,
+                        dept_name
+                    )
+
+                    countries.add_child(
                         colombia,
-                        department                                                      
+                        department
                     )
 
-                    departamentos[dept_code] = department
+                    departments[dept_code] = department
 
-                    city = City(
-                        city_code,
-                        city_name,
-                        lat,
-                        lon
-                    )
+                city = City(
+                    city_code,
+                    city_name,
+                    lat,
+                    lon
+                )
 
-                    multilista.add_child(
-                        departamentos[dept_code],
-                        city
-                    )
-            return multilista
+                countries.add_child(
+                    departments[dept_code],
+                    city
+                )
+        return countries
+
+
     def get_markers(self, multilist):
         markers = []
-
         current_country = multilist.head
-
         while current_country:
             if current_country.sub_list:
                 current_department = current_country.sub_list.head
-
                 while current_department:
                     if current_department.sub_list:
                         current_city = current_department.sub_list.head
-
                         while current_city:
-                            markers.append({
-                                "lat": current_city.lat,
-                                "lon": current_city.lon,
-                                "popup": f"""
-                                <b>{current_city.name}</b><br>
-                                Departamento:
-                                {current_department.name}
-                                """
-                                })   
+                            if (
+                                current_city.lat is not None and
+                                current_city.lon is not None
+                            ):
+
+                                markers.append({
+                                    "lat": current_city.lat,
+                                    "lon": current_city.lon,
+                                    "popup": f"""{current_city.name}"""
+                                })
 
                             current_city = current_city.next
 
